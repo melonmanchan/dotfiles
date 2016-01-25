fdfc23e16e1e4e0c5931b92885d2d644d2a826b8
Matti Jokitulppo <melonmanchan@gmail.com>
2016-01-25 17:20:18 +0200

    Install fonts system wide

--- bootstrap-vim.sh
+++ bootstrap-vim.sh
@@ -44,6 +44,6 @@ cd
 
 wget https://github.com/powerline/powerline/raw/develop/font/PowerlineSymbols.otf
 wget https://github.com/powerline/powerline/raw/develop/font/10-powerline-symbols.conf
-mv PowerlineSymbols.otf ~/.fonts/
-sudo fc-cache -vf ~/.fonts/
-mv 10-powerline-symbols.conf ~/.config/fontconfig/conf.d/
+sudo mv PowerlineSymbols.otf /usr/share/fonts/
+sudo fc-cache -vf
+sudo mv 10-powerline-symbols.conf /etc/fonts/conf.d/
