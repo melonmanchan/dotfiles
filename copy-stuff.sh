0bccc2a5c8c439c8c395c93737336e5d69c79a74
Matti Jokitulppo <melonmanchan@gmail.com>
2016-02-21 18:54:14 +0200

    Add spacemacs configuration

--- copy-stuff.sh
+++ copy-stuff.sh
@@ -7,6 +7,7 @@ cp .global_gitignore ~/.global_gitignore
 cp .tmux.conf        ~/.tmux.conf
 cp .gitconfig        ~/.gitconfig
 cp .ctags            ~/.ctags
+cp .spacemacs        ~/.spacemacs
 
 cp -r .config        ~/.config
 
