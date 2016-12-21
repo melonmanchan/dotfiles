0bfcb4b97816795201557e07ede19c4cb05cc96d
Matti Jokitulppo <melonmanchan@gmail.com>
2016-12-21 18:47:36 +0200

    Add automatic plugin loading to vim-plug script

--- install-vim-plug.sh
+++ install-vim-plug.sh
@@ -1,5 +1,8 @@
 #!/usr/bin bash
+set -e
 cp .vimrc ~/
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+vim +PlugInstall +qall
